78b0da5a166f889b047ec6cbfa4fe586b2153e6a
Daniyar771992 <daniyar771992@mail.ru>
2018-02-10 14:53:30 +0800

    create Calculate

---
+++ chapter_001/src/ru/job4j/Calculate.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+/**
+* Calculate
+*
+* @author Daniyar Nurgozhin
+*/
+public class Calculate {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
