2e9a369e2dbe38bde8dc8694dc38b882fe8637ae
Daniyar771992 <daniyar771992@mail.ru>
2018-02-25 19:35:03 +0800

    delete Calculate.java

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,16 @@
 package ru.job4j;
+
+/**
+ * Calculate
+ *
+ * @author Daniyar Nurgozhin
+ */
 public class Calculate {
+    
+	/**
+     * Main.
+     * @param args - args.
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
